b6b8a47b388e56140363ff1af897792e606f4ff4
Schickli <67188361+Schickli@users.noreply.github.com>
2024-11-23 18:11:27 +0100

    More Information

--- resume-builder/components/information-section.tsx
+++ resume-builder/components/information-section.tsx
@@ -15,6 +15,7 @@ export function InformationSection() {
         <ul className="list-disc list-inside space-y-2">
           <li>Please ensure that the profile image in your JSON resume data is square for optimal display.</li>
           <li>The resume layout is optimized for A4 paper size and PDF download.</li>
+          <li>Printing might not work in Safari and Firefox Android.</li>
           <li>For the best experience, use a modern web browser on the desktop.</li>
           <li>This resume builder was created using Next.js, shadcn/ui and Tailwind CSS.</li>
           <li>Created by <a href="http://schickli.com" target="_blank" className="text-blue-500">Maurin Schickli</a> - {new Date().getFullYear()}</li>
